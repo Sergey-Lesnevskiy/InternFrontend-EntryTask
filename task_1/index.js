525b3ac1cfbd169e2dce90c4bb6bb6c5e0c9d3f6
Lesnevsky-Sergey <78030028+Sergey-Lesnevskiy@users.noreply.github.com>
2024-05-09 16:45:32 +0300

    feat: add first task

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,34 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from "./data.js";
 
-console.log("Let's rock")
-console.log(encoded, translations)
+console.log("Let's rock");
 
+function encoding(encoded) {
+  const arr = [...encoded];
+  const uniq = []
+  arr.map((obj, count) => {
+    const key = Object.entries(obj);
+    key.forEach((item) => {
+      if (
+        item[0] === "groupId" ||
+        item[0] === "service" ||
+        item[0] === "formatSize" ||
+        item[0] === "ca"||
+        item[1] === null
+      ) {
+        return;
+      } else if (item[0].endsWith("Id")) {
+        if (translations[item[1]] || item[1] === '0') {
+          arr[count][item[0]] = translations[item[1]];
+        }
+      }   
+    });
+    key.forEach((item) => {
+      if(item[0].endsWith("Id")&&!translations[item[1]] && item[1] !== '0' && item[1] !== null){
+        uniq.push(item)
+      }
+    })
+  });
 
-
-// console.log(decoded)
+  return [...arr,...uniq];
+}
+console.log(encoding(encoded));
